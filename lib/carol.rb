737a84d4a91ff358f9ec455de0179795ebdbd17a
kamalogudah <mcpaul2058@gmail.com>
2018-05-11 09:24:25 +0300

    Remove carol-0.1.0.gem

--- lib/carol.rb
+++ lib/carol.rb
@@ -4,6 +4,7 @@ module Carol
   # Your code goes here...
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
       ["Hello from Ruby on Carol!"]]
     end
