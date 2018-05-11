30b212e4113025a2e2f92419ac49da3805a5b336
kamalogudah <mcpaul2058@gmail.com>
2018-05-11 09:12:00 +0300

    Added Rack

--- lib/carol.rb
+++ lib/carol.rb
@@ -2,4 +2,10 @@ require "carol/version"
 
 module Carol
   # Your code goes here...
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'},
+      ["Hello from Ruby on Carol!"]]
+    end
+  end
 end
