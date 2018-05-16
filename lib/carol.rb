500481b47f87efe2357aa22c7ab93f3d5cbd9ca7
kamalogudah <mcpaul2058@gmail.com>
2018-05-16 23:26:07 +0300

    Routing

--- lib/carol.rb
+++ lib/carol.rb
@@ -1,13 +1,28 @@
 require "carol/version"
 require "carol/array"
+require "carol/routing"
 
 module Carol
   # Your code goes here...
   class Application
     def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
-      ["Hello from Ruby on Carol!"]]
+      [text]]
+    end
+  end
+
+
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+    def env
+      @env
     end
   end
 end
