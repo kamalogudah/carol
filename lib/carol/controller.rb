3185c0c38261ac615650ab07a6d861718df28b3d
kamalogudah <mcpaul2058@gmail.com>
2018-05-18 12:36:58 +0300

    Added rendering to controller

--- lib/carol/controller.rb
+++ lib/carol/controller.rb
@@ -1,3 +1,4 @@
+require "erubis"
 module Carol
   class Controller
     def initialize(env)
@@ -5,6 +6,13 @@ module Carol
     end
     def env
       @env
+    end
+
+    def render(view_name, locals = {})
+      filename = File.join "app", "views","#{view_name}.html.erb"
+      template = File.read filename
+      eruby = Erubis::Eruby.new(template)
+      eruby.result locals.merge(:env => env)
     end
   end
 end
