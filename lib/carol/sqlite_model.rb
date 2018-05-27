a7b4ce682a0f7b069b11046907b596111a686612
kamalogudah <mcpaul2058@gmail.com>
2018-05-27 15:42:38 +0300

    Schema and Data

--- lib/carol/sqlite_model.rb
+++ lib/carol/sqlite_model.rb
@@ -4,8 +4,46 @@ DB = SQLite3::Database.new "test.db"
 module Carol
   module Model
     class SQLite
+      def initialize(data = nil)
+        @hash = data
+      end
+
+      def self.to_sql(val)
+        case val
+          when Numeric
+            val.to_s
+          when String
+            "'#{val}'"
+          else
+            raise "Can't change #{val.class} to SQL!"
+        end
+      end
+
+      def self.create(values)
+        values.delete "id"
+        keys = schema.keys - ["id"]
+        vals = keys.map do |key|
+          values[key] ? to_sql(values[key]) :
+            "null"
+        end
+        DB.execute <<~SQL
+         INSERT INTO #{table} (#{keys.join ","})
+         VALUES (#{vals.join ","});
+        SQL
+        data = Hash[keys.zip vals]
+        sql = "SELECT last_insert_rowid();"
+        data["id"] = DB.execute(sql)[0][0]
+        self.new data
+      end
+
+      def self.count
+        DB.execute(<<~SQL)[0][0]
+          SELECT COUNT(*) FROM #{table}
+        SQL
+      end
+
       def self.table
-        Carol.to_underscore name
+        Carol.to_underscore(name)
       end
 
       def self.schema
